68a5fa990d264bbf6f59afa46492d4be12f4cfc9
lucio-cornejo <luciocornejo11@gmailcom>
2022-07-16 07:30:17 -0500

    fixing variable "controls" scope issue

--- inst/htmlwidgets/plyLoadeR.js
+++ inst/htmlwidgets/plyLoadeR.js
@@ -14,8 +14,82 @@ HTMLWidgets.widget({
 
         // TODO: code to render the widget, e.g.
 
+        var renderer, camera, cameraTarget, controls, scene;
+
         init(x.path, el.id);
         animate();
+
+        function loadPLY(path, identifier) {
+          let loader = new THREE.PLYLoader();
+          loader.load(path, function (geometry) {
+            geometry.computeVertexNormals();
+            
+            let material = new THREE.MeshStandardMaterial({
+                wireframe: false,
+                opacity: 1,
+                transparent: true,
+                vertexColors: THREE.VertexColors
+              });
+            
+            let mesh = new THREE.Mesh(geometry, material);
+            // mesh.scale.multiplyScalar(0.035);
+                  
+            document.getElementById(identifier).firstChild.scene.add(mesh);
+          });
+        }
+        
+        function init(path, identifier) {
+          if (document.getElementById(identifier)) {
+          } else {
+            // renderer
+            renderer = new THREE.WebGLRenderer({ antialias: true });
+            renderer.setPixelRatio(window.devicePixelRatio);
+            renderer.setSize(window.innerWidth / 1.715, window.innerHeight / 1.715);
+            renderer.outputEncoding = THREE.sRGBEncoding;
+            renderer.shadowMap.enabled = true;
+            document.getElementById(identifier).appendChild( renderer.domElement );
+        
+            // Camera
+            camera = new THREE.PerspectiveCamera(
+              35, window.innerWidth / window.innerHeight,
+              1, 1000
+            );
+            camera.position.set(25, 25, 25);
+        
+            cameraTarget = new THREE.Vector3(0, 0, 0);
+            camera.lookAt(cameraTarget);
+        
+            // Camera controls
+            controls = new THREE.TrackballControls( camera, renderer.domElement );
+        
+            // Scene
+            scene = new THREE.Scene();
+            scene.background = new THREE.Color("rgb(10%, 10%, 10%)");
+            document.getElementById(identifier).firstChild.scene = scene;
+            
+            // Lights
+            // scene.add( new THREE.AxesHelper( 20 ) );
+            scene.add(new THREE.HemisphereLight("rgb(255, 255, 255)", "rgb(255, 255, 255)"));
+        
+            // resize
+            window.addEventListener("resize", onWindowResize, false);
+          }
+        
+          // Load PLY file
+          loadPLY(path, identifier);
+        }
+        
+        function onWindowResize() {
+          camera.aspect = window.innerWidth / window.innerHeight;
+          camera.updateProjectionMatrix();
+          renderer.setSize(window.innerWidth / 1.715, window.innerHeight / 1.715);
+        }
+        
+        function animate() {
+          requestAnimationFrame( animate );  
+          controls.update();
+          renderer.render(scene, camera);
+        }
 
       },
 
